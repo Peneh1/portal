c224f79249a22e441f74223f54864b77e29542fd
Joel Kasirer <82480771+Peneh1@users.noreply.github.com>
2022-10-24 18:45:12 -0400

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -36,9 +36,9 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
 
-                Route::domain('{subdomain}.' . env('APP_URL'))
-                ->middleware('web')
-                ->group(base_path('routes/subdomain.php'));
+            Route::domain('{subdomain}.' . env('APP_URL'))
+            ->middleware('web')
+            ->group(base_path('routes/subdomain.php'));
         });
     }
 
